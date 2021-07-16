1ac60b67d43bfcde87f511c7c8ca9651930aedde
nitoss <nitoss@mail.com>
2021-07-16 02:15:49 +0200

    libraries

---
+++ libraries/dataStructures/lineal/arrayQueue.java
@@ -0,0 +1,10 @@
+package libraries.dataStructures.lineal;
+
+//import libraries.dataStructures.models.Queue;
+/** Implementing queue interface throughout an Array 
+*@author (nitoss)
+*@version 1.0.0
+*@param <E> Array generic data type
+ */
+
+// public class arrayQueue<E> implements Queue<E>{}
