d1c6321f7c245cda67df7c8593653fd9f438f9ff
nitoss <nitoss@pop-os.localdomain>
2021-07-16 23:44:49 +0200

    changes

--- libraries/dataStructures/lineal/arrayQueue.java
+++ libraries/dataStructures/lineal/arrayQueue.java
@@ -7,4 +7,30 @@ package libraries.dataStructures.lineal;
 *@param <E> Array generic data type
  */
 
-// public class arrayQueue<E> implements Queue<E>{}
+public class arrayQueue<E> implements Queue<E>{
+    //Class atributes
+    protected static final int default_capacity  = 50;
+    //Instance atributes
+    protected E[] array; 
+    protected int end,first,size;
+    
+    //Constructor
+    @SuppressWarnings("unchecked")
+
+    public arrayQueue(){
+        array = (E[]) new Object[default_capacity];
+        first = 0;
+        end = -1;
+        size = 0;
+    }
+
+    public void queueUp(E e){
+        if(size == array.length){ expandQueue();}  //We make the array bigger so it fits in case it doesnt
+        end = increment(end);
+        array[end] = e;
+        size++;
+        
+    }
+
+
+}
